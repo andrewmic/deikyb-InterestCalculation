cb72036ed04825240578d13389b5f2e22ba7bfaf
andrewmic <37101387+andrewmic@users.noreply.github.com>
2018-03-15 15:14:30 +0200

    Review notes.

--- src/lt/itswedbankacade/interestcalculator/Main.java
+++ src/lt/itswedbankacade/interestcalculator/Main.java
@@ -23,26 +23,38 @@ public class Main {
 
         //Validation
         Scanner scanner = new Scanner(System.in);
+        //This way too complicated than it needs to be. It would be enough to validate fields in separate blocks instead of nesting.
         while (true) {
 
+            //"printf" should be used only when you want to format something. Here you should use "print" instead.
             System.out.printf("Amount: ");
+            //Amount is not protected from invalid number input (entering letters instead of number)
             double amount = scanner.nextDouble();
             if (amount <= 0) System.out.println("Invalid input! Try again!");
             else {
 
                 while (true) {
+                    //"printf" should be used only when you want to format something. Here you should use "print" instead.
                     System.out.printf("Period years: ");
                     try {
                         int periodYears = scanner.nextInt();
                         if (periodYears <= 0) System.out.println("Invalid input! Try again!");
                         else {
 
+                            //I see that you don't fully get how dynamic array expansion works. Follow these steps:
+                            // 1. Create a variable for an empty array: "double[] interestRates = new double[0];"
+                            // 2. Read input: "double rate = scanner.nextDouble();"
+                            // 3. Validate input: ...
+                            // 4. Expand array by 1 element: "interestRates = Arrays.copyOf(interestRates, interestRates.length + 1);"
+                            // 5. Add validated input at the end of expanded array: "interestRates[interestRates.length - 1] = rate;"
+                            // 6. Repeat
                             double[] newArr = new double[10000];
                             double[] interestRates;
                             int indexSize = 0;
 
                             do {
 
+                                //"printf" should be used only when you want to format something. Here you should use "print" instead.
                                 System.out.printf("Interest rate: ");
                                 double rate = scanner.nextDouble();
 
@@ -62,6 +74,7 @@ public class Main {
                             interestRates = Arrays.copyOf(newArr, indexSize);
 
                             while (true) {
+                                //"printf" should be used only when you want to format something. Here you should use "print" instead.
                                 System.out.printf("Compound frequency: ");
                                 String character = scanner.next();
                                 char period = character.charAt(0);
@@ -71,6 +84,7 @@ public class Main {
                                     double periodicly;
                                     periodicly = switchFrequency(period);
 
+                                    //You don't need to cast int to int ("(int) periodYears")
                                     int arraySize = (int) ((int) periodYears * periodicly);
                                     double[][] interimInterests = new double[arraySize][arraySize * interestRates.length];
 
@@ -85,6 +99,7 @@ public class Main {
                         }
                         break;
                     } catch (InputMismatchException e) {
+                        //"integral"?
                         System.out.println("Need number integral! Try again!");
                         scanner.next();
                     }
@@ -107,8 +122,12 @@ public class Main {
         }
     }
 
+    //I don't think it's a best idea to pass an empty "interimInterests" matrix as a parameter.
+    //It would be better to create it inside this method.
+    //See how "calculateInterimInerests" has a warning on the method name, so even Intellij is confused :)
     private static double[][] calculateInterimInerests(double[][] interimInterests, double[] interestRates, int periodYears, double periodicly, double amount) {
 
+        //"totalAmountOneLess" variable is redundant. It can be removed.
         double totalAmountOneLess;
         double totalAmount = 0;
 
@@ -152,6 +171,8 @@ public class Main {
                 return 4;
             case 'H':
                 return 2;
+            //"case 'Y':" and "default:" returns same result, so you can either remove last "return" and put "default:" above
+            // "case 'Y':", or remove "case 'Y':" block altogether.
             case 'Y':
                 return 1;
             default:
